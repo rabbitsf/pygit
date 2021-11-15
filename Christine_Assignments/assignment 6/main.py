fa6b7bf9c78af79136c6a9e56192a343f7f3d775
rabbitsf <lk_fung@yahoo.com>
2021-11-14 22:22:59 -0800

    This is a 2nd test on commiting using cli

--- Christine_Assignments/assignment 6/main.py
+++ Christine_Assignments/assignment 6/main.py
@@ -1,3 +1,5 @@
 # This is a test on the Git commit
 
 # I changed this file locally on the computer and we will see if it got changed after commit
+
+# This is 2nd commit test using the command line
