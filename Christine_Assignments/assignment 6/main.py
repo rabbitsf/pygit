9e1b59fb833279ed33e88f71f27122fd02b93410
rabbitsf <lk_fung@yahoo.com>
2021-11-14 21:29:47 -0800

    Test commit after editing local file

--- Christine_Assignments/assignment 6/main.py
+++ Christine_Assignments/assignment 6/main.py
@@ -1,16 +1,3 @@
-# This is a sample Python script.
+# This is a test on the Git commit
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# I changed this file locally on the computer and we will see if it got changed after commit
